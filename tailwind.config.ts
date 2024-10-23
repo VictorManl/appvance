b8c5a5d4c19490f33b9ba92045c65d394ab84db0
Victor manuel <victormel2003@gmail.com>
2024-10-23 12:36:57 -0500

    Actualizar dependencias y configurar Prettier con Tailwind CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        geist: "var(--font-geist)",
-        "space-grotesk": "var(--font-space-grotesk)",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: [
+		'./src/modules/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				geist: 'var(--font-geist)',
+				'space-grotesk': 'var(--font-space-grotesk)',
+			},
+		},
+	},
+	plugins: [],
+}
+export default config
