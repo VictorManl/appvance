57552278ce4b03aff5f9bd240dffc93e97fd0cee
Victor manuel <victormel2003@gmail.com>
2024-10-25 10:26:49 -0500

    Agregar validación de formularios y mejorar la gestión de errores en la página de contacto

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useMemo } from 'react'
-
+import { AnimatePresence, m } from 'framer-motion'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 
@@ -60,131 +60,162 @@ export default function Nav() {
 								{label}
 								{subLinks && <ArrowDown className="size-4" />}
 							</Link>
-							{subLinks && (
-								<nav className="hidden group-hover/item:block">
-									<ul className="border-border absolute left-0 mt-1.5 grid w-[840px] grid-cols-9 gap-x-4 rounded-lg border bg-white px-4 py-3 shadow-lg">
-										<div className="col-span-3 flex flex-col items-start justify-start gap-1">
-											<span className="text-[15px]">Web</span>
-											<div className="mt-3 flex w-full flex-col gap-1">
-												<Link href="/" className="group inline-flex gap-2 py-2">
-													<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
-														<Design className="size-5 text-inherit" />
-													</span>
-													<p className="flex flex-col">
-														<span className="text-[15px] text-foreground">
-															Diseño web
+							<AnimatePresence mode="wait">
+								{subLinks && (
+									<m.nav
+										className="absolute"
+										initial={{ opacity: 0 }}
+										animate={{ opacity: 1 }}
+										exit={{ opacity: 0 }}
+									>
+										<ul className="border-border absolute left-0 mt-1.5 grid w-[840px] grid-cols-9 gap-x-4 rounded-lg border bg-white px-4 py-3 shadow-lg">
+											<div className="col-span-3 flex flex-col items-start justify-start gap-1">
+												<span className="text-[15px]">Web</span>
+												<div className="mt-3 flex w-full flex-col gap-1">
+													<Link
+														href="/"
+														className="group inline-flex gap-2 py-2"
+													>
+														<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
+															<Design className="size-5 text-inherit" />
 														</span>
-														<span className="text-[13px]">
-															Diseñamos tu sitio web
+														<p className="flex flex-col">
+															<span className="text-[15px] text-foreground">
+																Diseño web
+															</span>
+															<span className="text-[13px]">
+																Diseñamos tu sitio web
+															</span>
+														</p>
+													</Link>
+													<Link
+														href="/"
+														className="group inline-flex gap-2 py-2"
+													>
+														<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
+															<Web className="size-5 text-inherit" />
 														</span>
-													</p>
-												</Link>
-												<Link href="/" className="group inline-flex gap-2 py-2">
-													<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
-														<Web className="size-5 text-inherit" />
-													</span>
-													<p className="flex flex-col">
-														<span className="text-[15px] text-foreground">
-															Aplicaciones web
+														<p className="flex flex-col">
+															<span className="text-[15px] text-foreground">
+																Aplicaciones web
+															</span>
+															<span className="text-[13px]">
+																Desarrollamos tu aplicación web
+															</span>
+														</p>
+													</Link>
+													<Link
+														href="/"
+														className="group inline-flex gap-2 py-2"
+													>
+														<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
+															<Software className="size-5 text-inherit" />
 														</span>
-														<span className="text-[13px]">
-															Desarrollamos tu aplicación web
+														<p className="flex flex-col">
+															<span className="text-[15px] text-foreground">
+																Software a la medida
+															</span>
+															<span className="text-[13px]">
+																Creamos tu software a la medida.
+															</span>
+														</p>
+													</Link>
+													<Link
+														href="/"
+														className="group inline-flex gap-2 py-2"
+													>
+														<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
+															<Dashboard className="size-5 text-inherit" />
 														</span>
-													</p>
-												</Link>
-												<Link href="/" className="group inline-flex gap-2 py-2">
-													<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
-														<Software className="size-5 text-inherit" />
-													</span>
-													<p className="flex flex-col">
-														<span className="text-[15px] text-foreground">
-															Software a la medida
-														</span>
-														<span className="text-[13px]">
-															Creamos tu software a la medida.
-														</span>
-													</p>
-												</Link>
-												<Link href="/" className="group inline-flex gap-2 py-2">
-													<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
-														<Dashboard className="size-5 text-inherit" />
-													</span>
-													<p className="flex flex-col">
-														<span className="text-[15px] text-foreground">
-															Administracion web
-														</span>
-														<span className="text-[13px]">
-															Nos encargamos de tu sitio web
-														</span>
-													</p>
-												</Link>
+														<p className="flex flex-col">
+															<span className="text-[15px] text-foreground">
+																Administracion web
+															</span>
+															<span className="text-[13px]">
+																Nos encargamos de tu sitio web
+															</span>
+														</p>
+													</Link>
+												</div>
 											</div>
-										</div>
-										<div className="col-span-3 flex flex-col items-start justify-start gap-1">
-											<span className="text-[15px]">Mobile</span>
-											<div className="mt-3 flex w-full flex-col gap-1">
-												<Link href="/" className="group inline-flex gap-2 py-2">
-													<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
-														<Mobile className="size-5 text-inherit" />
-													</span>
-													<p className="flex flex-col">
-														<span className="text-[15px] text-foreground">
-															Aplicacion
+											<div className="col-span-3 flex flex-col items-start justify-start gap-1">
+												<span className="text-[15px]">Mobile</span>
+												<div className="mt-3 flex w-full flex-col gap-1">
+													<Link
+														href="/"
+														className="group inline-flex gap-2 py-2"
+													>
+														<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
+															<Mobile className="size-5 text-inherit" />
 														</span>
-														<span className="text-[13px]">
-															Creamos tu aplicación móvil
+														<p className="flex flex-col">
+															<span className="text-[15px] text-foreground">
+																Aplicacion
+															</span>
+															<span className="text-[13px]">
+																Creamos tu aplicación móvil
+															</span>
+														</p>
+													</Link>
+													<Link
+														href="/"
+														className="group inline-flex gap-2 py-2"
+													>
+														<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
+															<MobileDev className="size-5 text-inherit" />
 														</span>
-													</p>
-												</Link>
-												<Link href="/" className="group inline-flex gap-2 py-2">
-													<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
-														<MobileDev className="size-5 text-inherit" />
-													</span>
-													<p className="flex flex-col">
-														<span className="text-[15px] text-foreground">
-															Administracion movil
-														</span>
-														<span className="text-[13px]">
-															Nos encargamos de tu aplicación móvil
-														</span>
-													</p>
-												</Link>
+														<p className="flex flex-col">
+															<span className="text-[15px] text-foreground">
+																Administracion movil
+															</span>
+															<span className="text-[13px]">
+																Nos encargamos de tu aplicación móvil
+															</span>
+														</p>
+													</Link>
+												</div>
 											</div>
-										</div>
-										<div className="col-span-3 flex flex-col items-start justify-start gap-1">
-											<span className="text-[15px]">Mejoras</span>
-											<div className="mt-3 flex w-full flex-col gap-1">
-												<Link href="/" className="group inline-flex gap-2 py-2">
-													<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
-														<Seo className="size-5 text-inherit" />
-													</span>
-													<p className="flex flex-col">
-														<span className="text-[15px] text-foreground">
-															SEO
+											<div className="col-span-3 flex flex-col items-start justify-start gap-1">
+												<span className="text-[15px]">Mejoras</span>
+												<div className="mt-3 flex w-full flex-col gap-1">
+													<Link
+														href="/"
+														className="group inline-flex gap-2 py-2"
+													>
+														<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
+															<Seo className="size-5 text-inherit" />
 														</span>
-														<span className="text-[13px]">
-															Mejoramos el SEO de tu sitio web
+														<p className="flex flex-col">
+															<span className="text-[15px] text-foreground">
+																SEO
+															</span>
+															<span className="text-[13px]">
+																Mejoramos el SEO de tu sitio web
+															</span>
+														</p>
+													</Link>
+													<Link
+														href="/"
+														className="group inline-flex gap-2 py-2"
+													>
+														<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
+															<WebRendering className="size-5 text-inherit" />
 														</span>
-													</p>
-												</Link>
-												<Link href="/" className="group inline-flex gap-2 py-2">
-													<span className="border-border flex size-10 items-center justify-center rounded-lg border text-black transition-all duration-100 ease-linear group-hover:bg-foreground group-hover:text-white">
-														<WebRendering className="size-5 text-inherit" />
-													</span>
-													<p className="flex flex-col">
-														<span className="text-[15px] text-foreground">
-															Renderezido
-														</span>
-														<span className="text-[13px]">
-															Tu sitio web más rápido y seguro
-														</span>
-													</p>
-												</Link>
+														<p className="flex flex-col">
+															<span className="text-[15px] text-foreground">
+																Renderezido
+															</span>
+															<span className="text-[13px]">
+																Tu sitio web más rápido y seguro
+															</span>
+														</p>
+													</Link>
+												</div>
 											</div>
-										</div>
-									</ul>
-								</nav>
-							)}
+										</ul>
+									</m.nav>
+								)}
+							</AnimatePresence>
 						</li>
 					))}
 				</ul>
